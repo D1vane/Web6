87ad0c2b57f961a1dbcf86442c459191f0029751
pablo <chayka.pavlik@mail.ru>
2024-05-23 14:52:50 +0700

    lab12

--- siteanimals/users/forms.py
+++ siteanimals/users/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.views import AuthenticationForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,PasswordChangeForm
 class LoginUserForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
@@ -27,3 +27,20 @@ class RegisterUserForm(UserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("Такой E-mail уже существует!")
         return email
+
+class ProfileUserForm(forms.ModelForm):
+    username = forms.CharField(disabled=True, label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.CharField(disabled=True, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'email', 'first_name', 'last_name']
+        labels = {'first_name': 'Имя', 'last_name': 'Фамилия', }
+        widgets = {'first_name': forms.TextInput(attrs={'class': 'form-input'}),
+                   'last_name': forms.TextInput(attrs={'class': 'form-input'}), }
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(label="Старый пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    new_password1 = forms.CharField(label="Новый пароль", widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    new_password2 = forms.CharField(label="Подтверждение пароля",
+                                    widget=forms.PasswordInput(attrs={'class': 'form-input'}))
--- siteanimals/users/views.py
+++ siteanimals/users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import LoginUserForm,RegisterUserForm
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.views import LoginView, LogoutView, AuthenticationForm
-from django.views.generic import CreateView
+from .forms import LoginUserForm,RegisterUserForm, ProfileUserForm,UserPasswordChangeForm
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.views import LoginView, LogoutView, AuthenticationForm,PasswordChangeView
+from django.views.generic import CreateView,UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class LoginUser(LoginView):
@@ -21,6 +22,28 @@ class RegisterUser(CreateView):
         'header' : "Регистрация"
     }
     success_url = reverse_lazy('users:login')
+
+class ProfileUser(LoginRequiredMixin,UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = 'users/profile.html'
+    extra_context = {
+        'title': "Профиль пользователя",
+        'header': "Профиль пользователя"
+    }
+    def get_success_url(self):
+        return reverse_lazy('users:profile',args=[self.request.user.pk])
+    def get_object(self, queryset=None):
+        return self.request.user
+
+class UserPasswordChange(PasswordChangeView):
+    form_class = UserPasswordChangeForm
+    success_url = reverse_lazy("users:password_change_done")
+    template_name = "users/password_change_form.html"
+    extra_context = {
+        'title' : "Изменение пароля",
+        'header': "Изменение пароля"
+    }
 def login_user(request):
     if request.method == 'POST':
         form = LoginUserForm(request.POST)
